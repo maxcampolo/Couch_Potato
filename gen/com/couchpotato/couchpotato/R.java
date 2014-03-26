f2fec17bc74cac680b0970b47635320895216f55
Max Campolo <m.campolo91@gmail.com>
2014-03-26 16:33:47 -0400

    profile layout without scrolling lists

--- gen/com/couchpotato/couchpotato/R.java
+++ gen/com/couchpotato/couchpotato/R.java
@@ -22,22 +22,31 @@ public final class R {
     }
     public static final class drawable {
         public static final int couch_potato_logo=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
+        public static final int couch_potato_logo_large=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int potato_icon=0x7f020003;
     }
     public static final class id {
-        public static final int action_settings=0x7f080008;
+        public static final int action_settings=0x7f08000e;
         public static final int button_login=0x7f080003;
         public static final int button_signup=0x7f080004;
-        public static final int button_singup=0x7f080007;
+        public static final int button_singup=0x7f08000d;
         public static final int editText_password=0x7f080002;
         public static final int editText_username=0x7f080001;
-        public static final int edittext_newpassword=0x7f080006;
-        public static final int edittext_newusername=0x7f080005;
+        public static final int edittext_newpassword=0x7f08000c;
+        public static final int edittext_newusername=0x7f08000b;
         public static final int imageview_logo=0x7f080000;
+        public static final int imageview_profileimage=0x7f080007;
+        public static final int textview_logout=0x7f08000a;
+        public static final int textview_schedule=0x7f080009;
+        public static final int textview_settings=0x7f080008;
+        public static final int textview_username=0x7f080005;
+        public static final int view_line1=0x7f080006;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int sign_up=0x7f030001;
+        public static final int fragment_profile=0x7f030001;
+        public static final int sign_up=0x7f030002;
     }
     public static final class menu {
         public static final int main=0x7f070000;
